fbcba2a92d4e17c3cc4528b329a7b9de6622d7f5
Mathias <mathias.2geroudet@gmail.com>
2020-01-18 22:33:21 +0100

    Plateau

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,5 +1,30 @@
 package Game;
 
+import java.util.Scanner;
+
 public class Game {
 
+	public static int nbrJoueur;
+	public int input;
+	Scanner scanner = new Scanner(System.in); 
+	
+	public void menu() {
+		System.out.println("Début d'une nouvelle partie de Robot Turtles ! \n"
+						+"Créer une parti de: "
+						+ "(1) 2 Joueurs"
+						+ "(2) 3 Joueurs"
+						+ "(3) 4 Joeurs");
+		
+	
+		input = scanner.nextInt();	
+		switch (input) {
+		case 1:
+			//completerProgramme();
+		case 2:
+			//construireMur();
+		case 3:
+			//executerProgramme();
+;			
+		}
+	}
 }
---
+++ src/Game/Plateau.java
@@ -0,0 +1,77 @@
+package Game;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Plateau {
+
+	int nbrJoueur = Game.nbrJoueur;
+	private char[][] plateauConsole = new char[8][8];
+	
+	public void Plateau()
+	{
+		switch (nbrJoueur) {
+		case 1:
+			plateau2Joueurs();
+		case 2:
+			plateau3Joueurs();
+		case 3:
+			plateau4Joueurs();
+;			
+		}
+	}
+	private void plateau2Joueurs(){
+		
+		int nbrCase = 0;
+		for(int y=0; y<8; y++){
+			for(int x=0; x<8; x++){
+				JButton btn = new JButton();
+				btn.setOpaque(false);
+				btn.setContentAreaFilled(false);
+				
+				if(x%7 == 0 && x!=0)
+				{					
+					plateauConsole[x][y] = 'M';					
+				}				
+				else if(nbrCase == 59)
+				{					
+					plateauConsole[x][y] = 'O';
+				}
+				else if(nbrCase == 1)
+				{
+					
+					plateauConsole[x][y] = '1';
+				}
+				else if(nbrCase == 5)
+				{
+					
+					plateauConsole[x][y] = '2';
+				}
+				else
+				{
+					plateauConsole[x][y] = '.';
+				}
+								
+				nbrCase+=1;
+				
+			}
+			
+		}
+		for(int y = 0 ; y < 8 ; y++){
+			System.out.print('|');
+			for(int x = 0 ; x < 8 ; x++){
+				System.out.print(" " + plateauConsole[x][y] + " ");
+			}
+			System.out.print('|');
+			System.out.println();}
+		
+	
+		
+	}
+	private void plateau3Joueurs(){
+		
+	}
+	private void plateau4Joueurs(){
+	
+}
+}
