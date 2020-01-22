45709bcfc4d5773f4844c24b81b9813a1e7c14b3
Mathias <mathias.2geroudet@gmail.com>
2020-01-22 22:58:13 +0100

    gh

--- src/Cartes/Cartes.java
+++ src/Cartes/Cartes.java
@@ -10,9 +10,8 @@ public class Cartes {
 	private String nomCarte;
 	
 	public enum Type {
-
+		
 		AVANCER("Bleu"), ROTATIONGAUCHE("Jaune"), ROTATIONDROITE("Violette"), LASER("Laser"), NULL("VIDE");
-
 		
 		private String nom;
 		
@@ -50,97 +49,36 @@ public class Cartes {
 					
 			if(direction == 1 && ligne < 8 ) 
 			{					
-				if(Plateau.plateauConsole[colonne-1][ligne] == "M" || Plateau.plateauConsole[colonne-1][ligne] == "G" ) {
-					tortue.setDirection(3);
-					System.out.println("Il y a un mur en face, vous faites demi tour !");
-					if(Game.Game.compteur == 1) {
-						tortue.setId("B3");
-					}else if(Game.Game.compteur == 2) {
-						tortue.setId("R3");
-					}else if(Game.Game.compteur == 3) {
-						tortue.setId("V3");
-					}else if(Game.Game.compteur == 4) {
-						tortue.setId("M3");
-					}
-					else if(Plateau.plateauConsole[colonne-1][ligne] != "." ) {
-						resetPosition(tortue);						
-				}else {
+				
 					tortue.setLigne(ligne + 1);
 					tortue.setNumeroCase(numeroCase + 8);		
-				}
-						
+				
 			}
+			
 			else if(direction == 2 && colonne > 0)
 			{
-				System.out.println("Direction 2");
-				if(Plateau.plateauConsole[colonne-2][ligne-1] == "M" ||Plateau.plateauConsole[colonne-2][ligne-1]== "G" ) {
-					tortue.setDirection(4);
-					System.out.println("Il y a un mur en face, vous faites demi tour !");
-					if(Game.Game.compteur == 1) {
-						tortue.setId("B4");
-					}else if(Game.Game.compteur == 2) {
-						tortue.setId("R4");
-					}else if(Game.Game.compteur == 3) {
-						tortue.setId("V4");
-					}else if(Game.Game.compteur == 4) {
-						tortue.setId("M4");
-					}
-					else if(Plateau.plateauConsole[colonne-2][ligne-1] != "." ) {
-						resetPosition(tortue);
-					}else {
+				
 					tortue.setColonne(colonne - 1);
 					tortue.setNumeroCase(numeroCase -1);		
-				}
+				
 			}
 			else if(direction == 3 && ligne > 0)
 			{
-				if(Plateau.plateauConsole[colonne+1][ligne] == "M" || Plateau.plateauConsole[colonne+1][ligne] == "G" ) {
-					tortue.setDirection(1);
-					System.out.println("Il y a un mur en face, vous faites demi tour !");
-					if(Game.Game.compteur == 1) {
-						tortue.setId("B1");
-					}else if(Game.Game.compteur == 2) {
-						tortue.setId("R1");
-					}else if(Game.Game.compteur == 3) {
-						tortue.setId("V1");
-					}else if(Game.Game.compteur == 4) {
-						tortue.setId("M1");
-					}
-					else if(Plateau.plateauConsole[colonne+1][ligne] != "." ) {
-						resetPosition(tortue);
-						
-				}else {
+				
 					tortue.setLigne(ligne -1);
 					tortue.setNumeroCase(numeroCase - 8);		
-				}
+				
 				
 			}
 			else if(direction == 4 && colonne < 8)
 			{
-				if(Plateau.plateauConsole[colonne][ligne-1] == "M" || Plateau.plateauConsole[colonne][ligne-1] == "G" ) {
-					tortue.setDirection(2);			
-					System.out.println("Il y a un mur en face, vous faites demi tour !");
-					if(Game.Game.compteur == 1) {
-						tortue.setId("B2");
-					}else if(Game.Game.compteur == 2) {
-						tortue.setId("R2");
-					}else if(Game.Game.compteur == 3) {
-						tortue.setId("V2");
-					}else if(Game.Game.compteur == 4) {
-						tortue.setId("M2");
-					}
-				}else if(Plateau.plateauConsole[colonne][ligne-1] != "." ) {
-					resetPosition(tortue);	
-				}else {
+				
 					tortue.setColonne(colonne + 1 );
 					tortue.setNumeroCase(numeroCase + 1);		
-				}
+				
 			}			
 			System.out.println("J'avance");
-					}
-				}
-			}
-		}
+
 		if(type == Type.ROTATIONGAUCHE) {
 			
 			if(direction == 1 )
@@ -211,7 +149,9 @@ public class Cartes {
 			*/
 				
 		}
+		}
 	}
+
 	private void resetPosition(Tortue tortue) {
 
 		int colonne = tortue.getColonne();
